8ffb4e22e4c350928ed37d7599c4c68f91916f34
kimo095 <80036845+kimo095@users.noreply.github.com>
2026-07-29 18:17:38 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,11 @@ import { SignupComponent } from "./auth/signup/signup.component";
   templateUrl: './app.component.html',
   imports: [LoginComponent, ReactiveFormLoginComponent, SignupComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  // Synchronous validator
+//(control) => null
+// Async validator
+//(control) => Observable<null | ValidationErrors>
+// So, a custom validator is about how you implement the validation rule, while an async validator is about the timing/async nature of the validation.
+//In fact, an async validator can also be a custom validator—the key difference is that it returns a Promise or Observable because it needs asynchronous work.
+}
